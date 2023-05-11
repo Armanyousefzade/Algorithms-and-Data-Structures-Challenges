c6373a9f9069850d1cfa5ec93efe57ee8cf544e6
ashooshtari <ashooshtari@benevity.com>
2023-05-11 02:05:21 -0700

    add longestConsecutive method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for (int number : nums) {
+            set.add(number);
+        }
+
+        int length;
+        int maxLength = 0;
+
+        for (int number : nums) {
+            length = 1;
+
+            while (set.contains(++number)) {
+                length++;
+            }
+
+            if (length > maxLength) {
+                maxLength = length;
+            }
+        }
+
+        return maxLength;
     }
 }
