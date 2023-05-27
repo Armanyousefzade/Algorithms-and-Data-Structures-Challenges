daa15e919604ce45d6a7ffabd0e0804538f11433
ashooshtari <armanyousefzade95@gmail.com>
2023-05-27 15:52:38 -0700

    Calculate the maximum depth of a binary tree

--- src/Main.java
+++ src/Main.java
@@ -385,4 +385,22 @@ public class Main {
         invertTree(root.left);
         return root;
     }
+
+    /**
+     * Calculates the maximum depth of a binary tree.
+     *
+     * @param root The root of the binary tree.
+     * @return The maximum depth of the binary tree.
+     */
+    public int maxDepth(TreeNode root) {
+        return maxDepth(root, 0);
+    }
+
+    private int maxDepth(TreeNode root, int depth) {
+        if (root == null) {
+            return depth;
+        }
+
+        return Math.max(maxDepth(root.left, depth + 1), maxDepth(root.right, depth + 1));
+    }
 }
