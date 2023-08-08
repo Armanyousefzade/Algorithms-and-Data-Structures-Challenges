d26a1a61db5eccf101fc936a40459bb5d6ae9278
ashooshtari <armanyousefzade95@gmail.com>
2023-08-08 00:55:28 -0700

    Implement an algorithm to add two numbers represented as linked lists

--- src/Main.java
+++ src/Main.java
@@ -958,4 +958,38 @@ public class Main {
 
         return result;
     }
+
+    /**
+     * Adds two numbers represented as linked lists. Each node in the linked list contains a single digit,
+     * and the digits are stored in reverse order, meaning that the 1's digit is at the head of the list.
+     *
+     * @param l1 The head of the first linked list.
+     * @param l2 The head of the second linked list.
+     * @return The head of the linked list that represents the sum of the numbers.
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            current.next = new ListNode(sum % 10);
+            carry = sum / 10;
+            current = current.next;
+        }
+
+        return dummyHead.next;
+    }
 }
